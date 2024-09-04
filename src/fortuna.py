572aa3c3b1430272969845c67e9f54d34a74cea0
valiantlynx <gormerykombo@gmail.com>
2024-09-04 10:01:56 +0200

    fotuna model done. all thats needed is documentation making the presentasjon pitch

---
+++ src/fortuna.py
@@ -0,0 +1,119 @@
+import json
+import random
+
+
+class FortunaModel:
+    def __init__(self, params_file="assets/best_params.json"):
+        self.best_params = None
+        self.best_mse = float('inf')
+        self.params_file = params_file
+
+        # Load existing parameters if they exist
+        self.load_params()
+
+    def fit(self, X, y, params):
+        """IT is a simple linear model 
+        e.g 
+            y = a * X1 + b * X2 + c
+
+        It then calculates the mean squared error.
+
+        Args:
+            X (Array(tuples)): A list of tuples where each tuple contains the independent parameters from our dataset
+                e.g [(d['Total Time (ms)'], d['Interval Time (ms)'])] = [(140, 140), (710, 570),..]
+            y (Array[int]): a list of target/result values for each of the tuples. its the heights in cm
+            params (_type_): The params that the fortuna model created, The best model/best params for now
+
+        Returns:
+            tuple: it return the predictions and the mean squared error. (mse, predictions)
+        """
+        a, b, c = params
+        predictions = [(a * x1 + b * x2 + c) for x1, x2 in X]
+        mse = self.mean_squared_error(predictions, y)
+        return mse, predictions
+
+    def mean_squared_error(self, predictions, y):
+        """calculate the mean squared err
+
+        Args:
+            predictions (Array[float]): array of the predictions. simply the results of the linear functions
+            y (Array[int]): a list of target/result values for each of the tuples. its the heights in cm
+
+        Returns:
+            mse: float representing the mean squared error
+                more info from: https://deepchecks.com/glossary/mean-square-error-mse/#:~:text=The%20MSE%20is%20the%20average,squaring%20the%20discrepancies%20is%20multifaceted.
+                MSE is measured in square units. Using squared units instead of natural data units makes the interpretation less obvious.
+
+                The objective of squaring the discrepancies is multifaceted.
+                Squaring the differences removes negative mean squared error differences and guarantees that the squared mean error is always larger than or equal to zero. The value is usually always positive. Only a model without any errors will have an MSE of zero. This does not occur in actuality.
+        """
+        errors = [(pred - actual) ** 2 for pred, actual in zip(predictions, y)]
+        mse = sum(errors) / len(errors)
+        return mse
+
+    def calibrate(self, data):
+        """Calibrates the model. and tries to create better parameter every time its called. if it finds the better model it saves the parameters.
+
+        Args:
+            data (List[dicts{}]): a List of Dicts containing containing all the collected data.
+                THe data is a List of this pydantic model:
+                '''python
+                    from pydantic import BaseModel, Field
+                    from typing import List
+
+                    class BounceData(BaseModel):
+                        Height: int = Field(..., description="The height of the bounce in centimeters")
+                        Bounce_Number: int = Field(..., alias="Bounce Number", description="The sequential number of the bounce")
+                        Total_Time: str = Field(..., alias="Total Time", description="The total time taken in 'HH:MM.SS' format")
+                        Interval_Time: str = Field(..., alias="Interval Time", description="The time interval between bounces in 'HH:MM.SS' format")
+                        Total_Time_ms: int = Field(..., alias="Total Time (ms)", description="The total time in milliseconds")
+                        Interval_Time_ms: int = Field(..., alias="Interval Time (ms)", description="The interval time in milliseconds")
+                '''
+        """
+     
+        X = [(d['Total Time (ms)'], d['Interval Time (ms)']) for d in data]
+        y = [d['Height'] for d in data]
+
+        for _ in range(1000):
+            params = [random.uniform(-1, 1) for _ in range(3)]
+            mse, _ = self.fit(X, y, params)
+
+            if mse < self.best_mse:
+                self.best_mse = mse
+                self.best_params = params
+                self.save_params()
+
+    def predict(self, X):
+        """Predicts the 
+
+        Args:
+            X (Array(tuples)): A list of tuples where each tuple contains the independent parameters from our dataset
+                e.g [(d['Total Time (ms)'], d['Interval Time (ms)'])] = [(140, 140), (710, 570),..]
+
+        Raises:
+            Exception: if the best params are not saved. just means you have to run self.calibrate(data) first
+
+        Returns:
+            Array[float]: array of the predictions
+        """
+        if self.best_params is None:
+            raise Exception("Model is not calibrated yet.")
+        a, b, c = self.best_params
+        print("a, b, c:", a, b, c)
+        print("Predictions:", [(a * x1 + b * x2 + c) for x1, x2 in X])
+        # Calculate The predictions
+        return [(a * x1 + b * x2 + c) for x1, x2 in X]
+
+    def save_params(self):
+        with open(self.params_file, 'w') as f:
+            json.dump({'best_params': self.best_params,
+                      'best_mse': self.best_mse}, f)
+
+    def load_params(self):
+        try:
+            with open(self.params_file, 'r') as f:
+                params = json.load(f)
+                self.best_params = params['best_params']
+                self.best_mse = params['best_mse']
+        except FileNotFoundError:
+            pass
--- src/main.py
+++ src/main.py
@@ -4,12 +4,11 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import pandas as pd
-import numpy as np
+from src.fortuna import FortunaModel
 import matplotlib.pyplot as plt
 import io
 import base64
-import json
-import random
+
 
 app = FastAPI()
 
@@ -18,43 +17,13 @@ app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 # Load existing bounce data from CSV file
 bounce_data_file = "src/tennis_ball_bounce_data.csv"
 bounce_data_df = pd.read_csv(bounce_data_file)
-
+full_bounce_data_df = pd.read_csv(bounce_data_file)
 # Consider only the first four bounces
 bounce_data_df = bounce_data_df[bounce_data_df['Bounce Number'] <= 4]
 
 # Convert CSV data to dictionary list format for model usage
 bounce_data = bounce_data_df.to_dict(orient='records')
-
-class FortunaModel:
-    def __init__(self):
-        self.best_params = None
-        self.best_mse = float('inf')
-
-    def fit(self, X, y, params):
-        # Simple linear model y = a * X1 + b * X2 + c
-        a, b, c = params
-        predictions = a * X[:, 0] + b * X[:, 1] + c
-        mse = np.mean((predictions - y) ** 2)
-        return mse, predictions
-
-    def calibrate(self, data):
-        X = np.array([[d['Total Time (ms)'], d['Interval Time (ms)']] for d in data])
-        y = np.array([d['Height'] for d in data])
-
-        for _ in range(1000):  # Try 1000 random parameter sets
-            params = [random.uniform(-1, 1) for _ in range(3)]  # Random coefficients a, b, c
-            mse, _ = self.fit(X, y, params)
-
-            if mse < self.best_mse:
-                self.best_mse = mse
-                self.best_params = params
-
-    def predict(self, X):
-        X = np.array(X)
-        if self.best_params is None:
-            raise Exception("Model is not calibrated yet.")
-        a, b, c = self.best_params
-        return a * X[:, 0] + b * X[:, 1] + c
+full_bounce_data = full_bounce_data_df.to_dict(orient='records')
 
 model = FortunaModel()
 model.calibrate(bounce_data)
@@ -69,44 +38,47 @@ async def read_root():
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>TenniScale</title>
             <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+            <link rel="stylesheet" href="/assets/styles.css">
             <script src="/assets/timer.js" defer></script>
         </head>
-        <body class="bg-blue-100 font-sans leading-normal tracking-normal">
+        <body class="bg-custom-olive font-sans leading-normal tracking-normal">
             <div class="container mx-auto p-4">
-                <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <h1 class="text-2xl font-bold mb-4 text-center text-blue-600">Welcome to TenniScale!</h1>
+                <div class="bg-custom-dark shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <h1 class="text-2xl font-bold mb-4 text-center text-custom-black">Welcome to TenniScale!</h1>
                     <div class="text-center mb-6">
-                        <button id="startButton" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Start Timer</button>
-                        <button id="stopButton" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" disabled>Stop Timer</button>
-                        <button id="recordBounceButton" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" disabled>Record Bounce</button>
+                        <button id="startButton" class="bg-custom-lime hover-bg-custom-dark text-custom-gray font-bold py-2 px-4 rounded">Start Timer</button>
+                        <button id="stopButton" class="bg-custom-olive hover-bg-custom-dark text-custom-gray font-bold py-2 px-4 rounded" disabled>Stop Timer</button>
+                        <button id="recordBounceButton" class="bg-custom-pale-olive hover-bg-custom-dark text-custom-gray font-bold py-2 px-4 rounded" disabled>Record Bounce</button>
                     </div>
                     <div class="text-center mb-6">
-                        <p>Elapsed Time: <span id="elapsedTime">0.00</span> seconds</p>
+                        <p class="text-custom-black">Elapsed Time: <span id="elapsedTime">0.00</span> seconds</p>
                     </div>
                     <!-- Form to submit data for calibration -->
                     <form action="/api/measure" method="post" class="text-center mb-6">
                         <input type="hidden" id="total_time_ms" name="total_time_ms">
                         <input type="hidden" id="interval_time_ms" name="interval_time_ms">
                         <div class="mb-4">
-                            <label for="height" class="block text-gray-700 text-sm font-bold mb-2">Height (cm):</label>
-                            <input type="number" id="height" name="height" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                            <label for="height" class="block text-custom-black text-sm font-bold mb-2">Height (cm):</label>
+                            <input type="number" id="height" name="height" class="shadow appearance-none border rounded w-full py-2 px-3 text-custom-black leading-tight focus:outline-none focus:shadow-outline" required>
                         </div>
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit Measurement for Calibration</button>
+                        <button type="submit" class="bg-custom-pale-olive hover-bg-custom-dark text-custom-gray font-bold py-2 px-4 rounded">Submit Measurement for Calibration</button>
                     </form>
                     <!-- Form to trigger prediction -->
                     <form action="/api/predict" method="post" class="text-center">
                         <input type="hidden" id="total_time_ms_predict" name="total_time_ms">
                         <input type="hidden" id="interval_time_ms_predict" name="interval_time_ms">
-                        <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Predict Height</button>
+                        <button type="submit" class="bg-custom-lime hover-bg-custom-dark text-custom-gray font-bold py-2 px-4 rounded">Predict Height</button>
                     </form>
                     <div class="text-center mt-6">
-                        <a href="/api/results" class="text-blue-500 hover:text-blue-800">View Results</a>
+                        <a href="/api/results" class="text-custom-black hover:text-custom-gray">View Results</a>
                     </div>
                 </div>
             </div>
         </body>
         </html>
+
     """
+
 
 @app.post("/api/measure")
 async def submit_data(height: float = Form(...), total_time_ms: float = Form(...), interval_time_ms: float = Form(...)):
@@ -118,16 +90,19 @@ async def submit_data(height: float = Form(...), total_time_ms: float = Form(...
     model.calibrate(bounce_data)
     return RedirectResponse(url="/", status_code=303)
 
+
 @app.get("/api/calibrate")
 async def calibrate_model():
     model.calibrate(bounce_data)
     return {"message": "Model calibrated successfully!"}
 
+
 @app.get("/api/results", response_class=HTMLResponse)
 async def get_results():
     df = pd.DataFrame(bounce_data)
+    full_df = pd.DataFrame(full_bounce_data)
 
-    # 1. Scatter Plot: Interval Time vs Total Time for Different Heights
+    # Scatter Plot: Interval Time vs Total Time for Different Heights
     fig1, ax1 = plt.subplots(figsize=(12, 6))
     for height in df['Height'].unique():
         subset = df[df['Height'] == height]
@@ -139,17 +114,14 @@ async def get_results():
         'Interval Time (ms) vs Total Time (ms) for Different Heights')
     ax1.legend()
     ax1.grid(True)
-
-    # Save first figure to buffer
     buf1 = io.BytesIO()
     plt.savefig(buf1, format='png')
     buf1.seek(0)
     img_str1 = base64.b64encode(buf1.read()).decode('utf-8')
-    plt.close(fig1)  # Close the figure to free up memory
+    plt.close(fig1)
 
-    # 2. Line Plots: Total Time and Interval Time Over Bounce Number for All Bounces
+    # Line Plots: Total Time and Interval Time Over Bounce Number for All Bounces
     fig2, (ax2, ax3) = plt.subplots(1, 2, figsize=(12, 6))
-
     for height in df['Height'].unique():
         height_data = df[df['Height'] == height]
         ax2.plot(height_data['Bounce Number'], height_data['Total Time (ms)'],
@@ -168,81 +140,95 @@ async def get_results():
     ax3.set_title('Interval Time vs Bounce Number')
     ax3.legend()
     ax3.grid(True)
-
     plt.tight_layout()
-
-    # Save second figure to buffer
     buf2 = io.BytesIO()
     plt.savefig(buf2, format='png')
     buf2.seek(0)
     img_str2 = base64.b64encode(buf2.read()).decode('utf-8')
-    plt.close(fig2)  # Close the figure to free up memory
-    
-    # 3. Histogram of Bounce Numbers
+    plt.close(fig2)
+
+    # Histogram of Bounce Numbers
     fig3, ax4 = plt.subplots(figsize=(12, 6))
-    max_bounce = int(df['Bounce Number'].max())  # Convert to int
-    ax4.hist(df['Bounce Number'], bins=range(
+    max_bounce = int(full_df['Bounce Number'].max())
+    ax4.hist(full_df['Bounce Number'], bins=range(
         1, max_bounce + 2), edgecolor='black')
     ax4.set_xlabel('Bounce Number')
     ax4.set_ylabel('Frequency')
     ax4.set_title('Histogram of Bounce Numbers')
     ax4.grid(True)
-
-    # Save third figure to buffer
     buf3 = io.BytesIO()
     plt.savefig(buf3, format='png')
     buf3.seek(0)
     img_str3 = base64.b64encode(buf3.read()).decode('utf-8')
-    plt.close(fig3)  # Close the figure to free up memory
+    plt.close(fig3)
 
-    # 4. Box Plot of Total Time and Interval Time
+    # Box Plot of Total Time and Interval Time
     fig4, ax5 = plt.subplots(figsize=(12, 6))
     df[['Total Time (ms)', 'Interval Time (ms)']].plot.box(ax=ax5)
     ax5.set_title('Box Plot of Total Time and Interval Time')
     ax5.grid(True)
-
-    # Save fourth figure to buffer
     buf4 = io.BytesIO()
     plt.savefig(buf4, format='png')
     buf4.seek(0)
     img_str4 = base64.b64encode(buf4.read()).decode('utf-8')
-    plt.close(fig4)  # Close the figure to free up memory
+    plt.close(fig4)
 
+    # Retrieve best params and MSE from the model
+    best_params = model.best_params
+    best_mse = model.best_mse
+
+    # Display the results including the best parameters and MSE
     return f"""
-    <html>
-    <head>
-        <title>Measurement Results</title>
-        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    </head>
-    <body class="bg-blue-100 font-sans leading-normal tracking-normal">
-        <div class="container mx-auto p-4">
-            <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <h1 class="text-2xl font-bold mb-4 text-center text-blue-600">Measurement Results</h1>
-                <div class="mb-4">
-                    <h2 class="text-xl font-semibold text-center text-blue-500">Interval Time vs Total Time for Different Heights</h2>
-                    <img src="data:image/png;base64,{img_str1}" class="mx-auto">
-                </div>
-                <div class="mb-4">
-                    <h2 class="text-xl font-semibold text-center text-blue-500">Total Time and Interval Time Over Bounce Number</h2>
-                    <img src="data:image/png;base64,{img_str2}" class="mx-auto">
-                </div>
-                <div class="mb-4">
-                    <h2 class="text-xl font-semibold text-center text-blue-500">Histogram of Bounce Numbers</h2>
-                    <img src="data:image/png;base64,{img_str3}" class="mx-auto">
-                </div>
-                <div class="mb-4">
-                    <h2 class="text-xl font-semibold text-center text-blue-500">Box Plot of Total Time and Interval Time</h2>
-                    <img src="data:image/png;base64,{img_str4}" class="mx-auto">
-                </div>
-                <br>
-                <div class="text-center">
-                    <a href="/" class="text-blue-500 hover:text-blue-800">Back</a>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Measurement Results</title>
+            <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+            <link rel="stylesheet" href="/assets/styles.css">
+        </head>
+        <body class="bg-custom-olive font-sans leading-normal tracking-normal">
+            <div class="container mx-auto p-4">
+                <div class="bg-custom-dark shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <h1 class="text-2xl font-bold mb-4 text-center text-custom-black">Measurement Results</h1>
+
+                    <div class="mb-4">
+                        <h2 class="text-xl font-semibold text-center text-custom-black">Interval Time vs Total Time for Different Heights</h2>
+                        <img src="data:image/png;base64,{img_str1}" class="mx-auto">
+                    </div>
+
+                    <div class="mb-4">
+                        <h2 class="text-xl font-semibold text-center text-custom-black">Total Time and Interval Time Over Bounce Number</h2>
+                        <img src="data:image/png;base64,{img_str2}" class="mx-auto">
+                    </div>
+
+                    <div class="mb-4">
+                        <h2 class="text-xl font-semibold text-center text-custom-black">Histogram of Bounce Numbers</h2>
+                        <img src="data:image/png;base64,{img_str3}" class="mx-auto">
+                    </div>
+
+                    <div class="mb-4">
+                        <h2 class="text-xl font-semibold text-center text-custom-black">Box Plot of Total Time and Interval Time</h2>
+                        <img src="data:image/png;base64,{img_str4}" class="mx-auto">
+                    </div>
+
+                    <div class="mb-4">
+                        <h2 class="text-xl font-semibold text-center text-custom-black">Best Parameters and MSE</h2>
+                        <p class="text-center text-lg text-custom-black">Best Parameters: {best_params}</p>
+                        <p class="text-center text-lg text-custom-black">Best MSE: {best_mse:.4f}</p>
+                    </div>
+
+                    <div class="text-center">
+                        <a href="/" class="text-custom-black hover-text-custom-dark">Back</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </body>
-    </html>
+        </body>
+        </html>
+
     """
+
 
 @app.post("/api/predict")
 async def predict_height(total_time_ms: float = Form(...), interval_time_ms: float = Form(...)):
@@ -258,21 +244,23 @@ async def predict_height(total_time_ms: float = Form(...), interval_time_ms: flo
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Prediction Result</title>
             <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+            <link rel="stylesheet" href="/assets/styles.css">
         </head>
-        <body class="bg-blue-100 font-sans leading-normal tracking-normal">
+        <body class="bg-custom-olive font-sans leading-normal tracking-normal">
             <div class="container mx-auto p-4">
-                <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <h1 class="text-2xl font-bold mb-4 text-center text-blue-600">Prediction Result</h1>
+                <div class="bg-custom-dark shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <h1 class="text-2xl font-bold mb-4 text-center text-custom-black">Prediction Result</h1>
                     <div class="text-center">
-                        <p class="text-lg font-semibold">Predicted Height: {predicted_height:.2f} cm</p>
+                        <p class="text-lg font-semibold text-custom-black">Predicted Height: {predicted_height:.2f} cm</p>
                     </div>
                     <div class="text-center mt-6">
-                        <a href="/" class="text-blue-500 hover:text-blue-800">Back</a>
+                        <a href="/" class="text-custom-black hover-text-custom-dark">Back</a>
                     </div>
                 </div>
             </div>
         </body>
         </html>
+
     """)
 
 # Run FastAPI
